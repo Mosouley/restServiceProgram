e0a52cc4c8c2d3dccc4155488c4275877db599fb
mosouley <mo.souley@gmail.com>
2017-11-14 15:22:46 +0100

    reprise git init

---
+++ src/main/java/com/bootcamp/rest/controllers/ProgrammeRessource.java
@@ -0,0 +1,276 @@
+/**
+ * signature Programme
+ */
+package com.bootcamp.rest.controllers;
+
+
+
+import com.bootcamp.entities.Programme;
+import com.bootcamp.jpa.ProgrammeRepository;
+import com.bootcamp.model.Programmes;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Link;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ *
+ * @author soul
+ */
+@Path("v0/programmes")
+@Api(value = "programmes", description = "web service on all the programs available")
+public class ProgrammeRessource {
+
+
+     /**
+      * Need to instantiate a repository to retrieve datas
+      * based on the persistence unit create
+      */
+    ProgrammeRepository programmeRepository = new ProgrammeRepository("databasePU");
+
+    //Annotation JAX-RS2 that helps gather all the injected info
+    @Context
+    public UriInfo uriInfo;
+
+    /**
+     *URI giving the access to the whole signature
+     * @param start
+     * @param size
+      * @return
+     * @throws java.sql.SQLException
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "To retrieve all the programmes and scroll to the previous and the next thanks to Hateoas")
+    public Programmes getProgrammes(@ApiParam(value = "start",required = true)  @QueryParam("start") int start,                  //
+            @QueryParam("size") @DefaultValue("2") int size  ) throws SQLException{
+
+       
+            /**
+             * //definition des URI permettant d'obtenir le previous et le next de
+             * tout programme aussi les parametres de pagination sont inclus
+             * dans la requete http avec un minimum de 2
+             */
+            //Utilisation de la classe abstraite uribuilder
+            
+            UriBuilder builder = uriInfo.getAbsolutePathBuilder();
+            builder.queryParam("start", "{start}");
+            builder.queryParam("size", "{size}");
+
+            //Method to access all the program
+            List<Programme> programmes;
+
+            programmes = programmeRepository.findAll();
+
+
+            //a collection of links to use hateaos
+            ArrayList<Link> links = new ArrayList<>();
+            //creation d'une liste de programme, constituee seulement des programmes
+            //respectant les criteres de start and size
+            ArrayList<Programme> list = new ArrayList<>();
+
+            //on fait remplir la liste des programmes respectant les queryparam de facon synchronisee pour eviter les appels concurrents
+            
+            synchronized (programmes) {
+                int i = 0;
+                for (Programme programme : programmes) {
+                    //on ajoute les programmes un a un a la liste jusqu'a ce que la taille definie soit  atteinte
+                    if (i >= start && i < start + size) {
+                        list.add(programme);
+                    }
+                    i++;
+                }
+            }
+
+            // next link
+            if (start + size < programmes.size()) {
+                int next = start + size;
+                URI nextUri = builder.clone().build(next, size);
+                Link nextLink = Link.fromUri(nextUri)
+                        .rel("next").type("application/json").build();
+                links.add(nextLink);
+            }
+            // previous link
+            if (start > 0) {
+                int previous = start - size;
+                if (previous < 0) {
+                    previous = 0;
+                }
+                URI previousUri = builder.clone().build(previous, size);
+                Link previousLink = Link.fromUri(previousUri)
+                        .rel("previous")
+                        .type("application/json").build();
+                links.add(previousLink);
+            }
+
+            //Pour chaque programme definir son link
+            //mise en oeuvre de l'implementation Hateoas
+            //boucle for sur chaque programme
+            //Introduire son champ self (lui-meme) qui fait reference a son lien
+            for (Programme programme : programmes) {
+                //pour chaque bailleur dans la liste
+                //trouver l'URI vers sa ressource provenant de la recherche par id se trouvant dans la methode getbyId
+
+                Link lien = Link.fromUri(uriInfo.getBaseUriBuilder()
+                        .path(getClass())       //permet d'avoir l'uri de base
+                        .path(getClass(), "getById")        //permet d'avoir l'URI de la methode recherche
+                        .build(programme.getReference()))   //ajoute la reference du programme comme queryparam
+                        .rel(programme.getNom())
+                        .type("GET").build();               //faire le build pour resoudre le lien
+                //definir le lien du programme ainsi obtenu
+                programme.setSelf(lien);
+
+                //pour chaque programme trouve lier le programme a son lien
+                Response.accepted(programme)
+                        .links(programme.getSelf())
+                        .build();
+                //setter pour fixer le lien vers cette ressource
+                programme.setSelf(lien);
+            }
+
+            Programmes listProgrammes = new Programmes();
+            //on la remplit de la liste provenant de la base de donnee
+            listProgrammes.setProgrammes(list);
+            //on lui met ses liens
+            listProgrammes.setLinks(links);
+            //on retourne la reponse a la requete
+            return listProgrammes;
+        
+ 
+    }
+
+    @GET
+    @Path("/programme/{ref}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getById(@PathParam("ref") int ref) throws SQLException {
+
+        Programme programme = programmeRepository.findByPropertyUnique("reference", ref);
+
+        if (programme != null) {
+            programme.setSelf(
+                    Link.fromUri(uriInfo.getAbsolutePath())
+                    .rel("self")
+                    .type("GET")
+                    .build());
+            return Response.accepted(programme).links(programme.getSelf()).build();
+        } else {
+            return Response.status(404).entity(programme).build();
+        }
+    }
+
+    /**
+     * methode de recherche sur un nombre limites de champs
+     * @param ref
+     * @param fields
+     * @return
+     * @throws SQLException
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     * @throws IntrospectionException
+     * @throws InvocationTargetException
+     */
+    @GET
+    @Path("/programme/param/{ref}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getByIdParam(@PathParam("ref") int ref, @QueryParam("fields") String fields) throws SQLException, IllegalArgumentException, IllegalAccessException, IntrospectionException, InvocationTargetException {
+       //Trouver les champs de recherche Separes par une virgule
+        String[] fieldArray = fields.split(",");
+
+        //retrouver le seul programme dont la reference est
+        Programme programme = programmeRepository.findByPropertyUnique("reference", ref);
+        //  Definissons un Map
+        Map<String, Object> responseMap = new HashMap<>();
+
+        //meta programming permettant d'obtenir tous les champs declares sur la classe programme
+        PropertyDescriptor[] propertyDescriptors = Introspector.getBeanInfo(Programme.class).getPropertyDescriptors();
+
+        //pour chaque proprete ou attribut parcourir ses methodes getter
+        for (PropertyDescriptor propertyDescriptor : propertyDescriptors) {
+
+            Method method = propertyDescriptor.getReadMethod();
+
+            //si la verification est concluante, donc le client souhaite obtenir ce champ
+            if (check(fieldArray, propertyDescriptor.getName())) {
+                responseMap.put(propertyDescriptor.getName(), method.invoke(programme));
+            }
+
+        }
+
+        //A la fin de la boucle le Map contiendra tous les champs demandes leur nom, et le resultat de leur methode getter
+        //on retourne donc la tableau de chmap dans la requete
+        if(responseMap.isEmpty()){      //Aucun des champs indiques n'appartient au service programme
+            return Response.noContent().build();
+        }else{
+        return Response.status(200).entity(responseMap).build();
+        }
+    }
+
+    @POST
+    @Path("/create")
+//    @Produces(MediaType.)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response create(Programme programme) {
+        String output = " Felicitations objet cree avec succes : ";
+        try {
+            programmeRepository.create(programme);
+            return Response.status(200).entity(output + programme.getNom()).build();
+        } catch (SQLException ex) {
+            return Response.status(404).entity("Erreur: Objet non cree").build();
+        }
+    }
+//
+    @PUT
+    @Path("/update")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update(Programme programme) {
+        String output = " Felicitations Mise a jour effectuee avec succes pour : ";
+        try {
+            programmeRepository.update(programme);
+            return Response.status(200).entity(output + programme.getNom()).build();
+        } catch (SQLException ex) {
+            return Response.status(404).entity("Erreur: Objet non mis a jour").build();
+        }
+
+    }
+//
+    /**
+     * methode permettant de verifier si un attribut fait partie d'une liste de champ indiques
+     * @param fields
+     * @param field
+     * @return
+     */
+    private boolean check(String[] fields, String field) {
+
+        for (String field1 : fields) {
+            if (field.equals(field1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/test/java/com/bootcamp/rest/tests/GenerateTableTest.java
@@ -0,0 +1,104 @@
+/**
+ * Realisation de quelques tests
+ */
+package com.bootcamp.rest.tests;
+
+
+import com.bootcamp.entities.PhaseProgramme;
+import com.bootcamp.entities.Programme;
+import com.bootcamp.enums.EtatProgramme;
+import com.bootcamp.rest.controllers.ProgrammeRessource;
+import com.bootcamp.service.crud.PhaseProgrammeCRUD;
+import java.beans.IntrospectionException;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author soul
+ */
+public class GenerateTableTest  {
+ 
+//we make no test for retrieving programmes because the resource uses uriinfo
+    //which can not be used out of a servlet context and can only be test using a client
+
+     @Test
+    public void getServiceProgrammeById() throws SQLException{
+        ProgrammeRessource uriProg=new ProgrammeRessource();
+        try {
+            //get the entire specified data for program 1
+            System.out.println("contenu demande: " + uriProg.getByIdParam(1, "nom,description").toString());
+        } catch (IllegalArgumentException | IllegalAccessException | IntrospectionException | InvocationTargetException ex) {
+            Logger.getLogger(GenerateTableTest.class.getName()).log(Level.SEVERE, null, ex);
+           
+        }
+    }
+
+    @Test
+    public void postServiceProgramme(){
+    
+      //initialisation de la signature  
+     ProgrammeRessource uriProg=new ProgrammeRessource();
+     
+     String nom="Luttre contre violence conjugale via web service";
+
+     String description="zero tolerance aux violences faites aux femmes via webservice"; //description du programme
+
+     int idObjectif=3; //id to recevoir permettant de faire des recherche sur un objectif precis
+
+     double budprevionnel=100000000;
+
+     double coutreel=300000000;
+
+    Date dateDebutPrev= new Date(2014/01/01);
+
+   Date dateFinPrev=new Date(2014/12/31);
+
+    Date dateDebutReel=new Date(2014/03/01);
+
+    Date dateFinReel=new Date(2015/06/01);
+
+    PhaseProgramme nPhase1=new PhaseProgramme();
+    nPhase1.setNomPhase("Phase cree depuis le web service");
+    PhaseProgramme nPhase2=new PhaseProgramme();
+    nPhase1.setNomPhase("Phase cree depuis le web service 2");
+    PhaseProgramme nPhase3=new PhaseProgramme();
+    nPhase1.setNomPhase("Phase cree depuis le web service 3");
+    PhaseProgramme nPhase4=new PhaseProgramme();
+    nPhase1.setNomPhase("Phase cree depuis le web service 4");
+    List<PhaseProgramme> nomPhases=new ArrayList<>();
+    //add some phases tu this project
+        PhaseProgrammeCRUD.create(nPhase1);
+        PhaseProgrammeCRUD.create(nPhase2);
+        PhaseProgrammeCRUD.create(nPhase3);
+        PhaseProgrammeCRUD.create(nPhase4);
+
+
+    PhaseProgramme phaseActuelle=nPhase2; // the programme is at begenning
+
+    EtatProgramme etatProgramme=EtatProgramme.EXECUTION;
+
+        Programme nvoProg=new Programme();
+        nvoProg.setBudprevionnel(budprevionnel);
+        nvoProg.setNom(nom);
+        nvoProg.setDescription(description);
+        nvoProg.setPhaseActuelle(phaseActuelle);
+        nvoProg.setIdObjectif(idObjectif);
+        nvoProg.setCoutreel(coutreel);
+        nvoProg.setDateDebutPrev(dateDebutPrev);
+        nvoProg.setDateDebutReel(dateDebutReel);
+        nvoProg.setDateFinPrev(dateFinPrev);
+        nvoProg.setDateFinReel(dateFinReel);
+        nvoProg.setNomPhases(nomPhases);
+        nvoProg.setEtatProgramme(etatProgramme);
+        uriProg.create(nvoProg);
+        
+   
+    }
+}
